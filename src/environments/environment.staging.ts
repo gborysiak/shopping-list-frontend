7e462362a9d270c4760f508b8fcb678168d521bb
gborysiak <gregory@borysiak.com>
2025-12-06 15:38:45 +0100

    fin modif angular 20

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  webserviceurl: "https://shopping-dev.borysiak.com/api"
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   //webserviceurl: "https://shopping-dev.borysiak.com/api"
-  //webserviceurl: "https://localhost:7279/api"
-  webserviceurl: "http://127.0.0.1:8010"
+  webserviceurl: "https://localhost:7279/api"
+  //webserviceurl: "http://127.0.0.1:8010"
 };
